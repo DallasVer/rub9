9442611935972be82f4b39af02337b8b1127a675
DallasVer <118686739+DallasVer@users.noreply.github.com>
2023-06-17 20:26:56 +0700

    Add files via upload

---
+++ Rub9/Rub9/FitnessCenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rub9
+{
+    internal class FitnessCenter
+    {
+        public int ClientID { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int TrainingDuration { get; set; }
+
+        public FitnessCenter(int clientID, int year, int month, int trainingDuration)
+        {
+            ClientID = clientID;
+            Year = year;
+            Month = month;
+            TrainingDuration = trainingDuration;
+        }
+    }
+}
---
+++ Rub9/Rub9/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rub9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Зад 1.
+            /*string[] array = { "Gfgdk", "dfkskf", "ii", "rtr", "dffa", "Greg", "kill", "Amper", "cxs", "iiIIopdsfo" };
+            var Lengths = array.Sum(ar => ar.Length);
+            Console.WriteLine(Lengths);*/
+
+            //Зад 2.
+            /*int[] nums = {7, -12, 3, 4, 25, 6, 3, -3, 95, 66};
+            var pluss = nums.Where( n => n > 0);
+
+            foreach( int n in pluss )
+            {
+                Console.Write(n + " ")
+            }*/
+
+            //Зад 3.
+            /*int[] array = { 5, 14, 9, -22, 10, 52, 17, -2, 100, 73 };
+            var sort = array.Where(n => n > 9 && n < 100);
+
+            foreach ( var item in sort )
+            {
+                Console.Write(item + " ");
+            }*/
+
+            //Зад 4.
+            /*var fitness = new List<FitnessCenter>()
+            {
+                new FitnessCenter(3972, 2011, 7, 3),
+                new FitnessCenter(5011, 2012, 12, 2),
+                new FitnessCenter(3712, 2011, 3, 4),
+                new FitnessCenter(1742, 2009, 7, 2),
+                new FitnessCenter(4260, 2012, 5, 1)
+            };
+
+            var LongTraining = fitness.Min(n => n.TrainingDuration);
+            var MaxTraining = fitness.Max(n => n.TrainingDuration);
+
+            Console.WriteLine($"Cамое продолжительное занятие {LongTraining} \n" +
+                $"Cамое короткое занятие {MaxTraining}");*/
+
+            //Зад 5.
+            var fitness = new List<FitnessCenter>()
+            {
+                new FitnessCenter(3972, 2011, 7, 3),
+                new FitnessCenter(5011, 2012, 12, 2),
+                new FitnessCenter(3712, 2011, 3, 4),
+                new FitnessCenter(1742, 2009, 7, 2),
+                new FitnessCenter(2666, 2010, 7, 3),
+                new FitnessCenter(1103, 2009, 1, 2),
+                new FitnessCenter(3941, 2011, 4, 1),
+                new FitnessCenter(1408, 2009, 4, 1),
+                new FitnessCenter(4872, 2012, 6, 5),
+                new FitnessCenter(4260, 2012, 5, 1)
+            };
+
+            var SumYear = fitness.GroupBy(n => n.Year).Select(g => new {Year = g.Key, DurationSum = g.Sum(x => x.TrainingDuration)}).OrderByDescending(n => n.DurationSum).ThenBy(n => n.Year).First();
+
+            Console.WriteLine($"Год с сумарно наибольшей продолжительностью всех занятий = {SumYear.Year}, сумарная сумма = {SumYear.DurationSum}");
+            
+        }
+    }
+} 
